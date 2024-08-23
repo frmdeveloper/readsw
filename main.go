7d912976d52f984999b189adf72cff2f421a6e58
frmdeveloper <fauzanrifkim@outlook.co.id>
2024-08-23 18:47:05 +0700

    Update baru

--- main.go
+++ main.go
@@ -1,88 +1,81 @@
 package main
 
 import (
-	"context"
-	"golang.org/x/net/webdav"
-	"net/http"
-	"fmt"
-	"go.mau.fi/whatsmeow"
-	"go.mau.fi/whatsmeow/store/sqlstore"
-	"go.mau.fi/whatsmeow/types"
-	"go.mau.fi/whatsmeow/types/events"
-	"os"
-	"strings"
-	"time"
-	waLog "go.mau.fi/whatsmeow/util/log"
-	waProto "go.mau.fi/whatsmeow/binary/proto"
-	_ "github.com/mattn/go-sqlite3"
+    "context"
+    "flag"
+    "fmt"
+    "go.mau.fi/whatsmeow"
+    "go.mau.fi/whatsmeow/store/sqlstore"
+    "go.mau.fi/whatsmeow/types"
+    "go.mau.fi/whatsmeow/types/events"
+    "os"
+    "os/signal"
+    "syscall"
+    waLog "go.mau.fi/whatsmeow/util/log"
+    _ "github.com/ncruces/go-sqlite3/driver"
+    _ "github.com/ncruces/go-sqlite3/embed"
 )
 
-func main() {
-NewBot("62xxxxxxxx", func(k string) { //ganti nomormu disitu
-	println(k)
-})
-	http.Handle("/file/", http.StripPrefix("/file", &webdav.Handler{
-		FileSystem: webdav.Dir("."),
-		LockSystem: webdav.NewMemLS(),
-	}))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("awokawok"))
-	})
-	erro := http.ListenAndServe(":8080", nil)
-	if erro != nil {
-		println("HTTP ERROR",erro)
-	}
+type Ev struct {
+    ChatPresence *events.ChatPresence
+    Message *events.Message
+    Receipt *events.Receipt
+    More interface {}
+}
+func Connect(nomor string, cb func(conn *whatsmeow.Client, evt Ev)) {
+    dbLog := waLog.Stdout("Database", "ERROR", true)
+    container, err := sqlstore.New("sqlite3", "file:"+nomor+".db?nolock=1", dbLog)
+    if err != nil { fmt.Println("GoError:",err); return }
+    deviceStore, err := container.GetFirstDevice()
+    if err != nil { fmt.Println("GoError:",err); return }
+    clientLog := waLog.Stdout("Client", "ERROR", true)
+    client := whatsmeow.NewClient(deviceStore, clientLog)
+    client.AddEventHandler(func(evt interface {}) {
+      switch evt.(type) {
+        case *events.Message:
+          cb(client, Ev{Message:evt.(*events.Message)})
+        case *events.Receipt:
+          cb(client, Ev{Receipt:evt.(*events.Receipt)})
+        case *events.ChatPresence:
+          cb(client, Ev{ChatPresence:evt.(*events.ChatPresence)})
+        default:
+          cb(client, Ev{More:evt})
+      }
+    })
+    if client.Store.ID == nil {
+        err = client.Connect()
+        if err != nil { fmt.Println("GoError:",err); return }
+        linkingCode, gagal := client.PairPhone(nomor, true, whatsmeow.PairClientChrome, "Chrome (Linux)")
+        if gagal != nil { fmt.Println("GoError:",gagal); return }
+        fmt.Println(nomor,">",linkingCode)
+    } else {
+        err = client.Connect()
+        if err != nil { fmt.Println("GoError:",err); return }
+        fmt.Println(nomor,">","Connected")
+        //client.SendPresence(types.PresenceAvailable)
+        client.SendPresence(types.PresenceUnavailable)
+    }
 }
 
-func registerHandler(client *whatsmeow.Client) func(evt interface{}) {
-  return func(evt interface{}) {
-	switch v := evt.(type) {
-		case *events.Message:
-			if v.Info.Chat.String() == "status@broadcast" {
-				client.MarkRead([]types.MessageID{v.Info.ID}, v.Info.Timestamp, v.Info.Chat, v.Info.Sender)
-			}
-			if v.Message.GetConversation() == "minta readsw" {
-				NewBot(v.Info.Sender.String(), func(k string) {
-					client.SendMessage(context.Background(), v.Info.Sender, &waProto.Message{
-						ExtendedTextMessage: &waProto.ExtendedTextMessage{
-							Text: &k,
-						},
-					}, whatsmeow.SendRequestExtra{})
-				})
-			}
-		}
-	}
+func mes(client *whatsmeow.Client, evt Ev) {
+    if evt.Message != nil {
+        v := evt.Message
+        if v.Info.Chat.String() == "status@broadcast" {
+            if v.Info.Type != "reaction" {
+                reaction := client.BuildReaction(v.Info.Chat, v.Info.Sender, v.Info.ID, "🗿")
+                extras := []whatsmeow.SendRequestExtra{}
+                client.MarkRead([]types.MessageID{v.Info.ID}, v.Info.Timestamp, v.Info.Chat, v.Info.Sender)
+                client.SendMessage(context.Background(), v.Info.Chat, reaction, extras...)
+            }
+        }
+    }
 }
-func NewBot(id string, callback func(string)) *whatsmeow.Client {
-  if id == "" { callback("Nomor ?"); return nil }
-  id = strings.ReplaceAll(id, "admin", "")
-  dbLog := waLog.Stdout("Database", "ERROR", true)
-  container, err := sqlstore.New("sqlite3", "file:data/"+id+".db?_foreign_keys=on", dbLog)
-  if err != nil {
-	callback("Kesalahan (error)\n"+fmt.Sprintf("%s",err)); return nil
-  }
-  deviceStore, err := container.GetFirstDevice()
-  if err != nil {
-	callback("Kesalahan (error)\n"+fmt.Sprintf("%s",err)); return nil
-  }
-  clientLog := waLog.Stdout("Client", "ERROR", true)
-  client := whatsmeow.NewClient(deviceStore, clientLog)
-  client.AddEventHandler(registerHandler(client))
-  err = client.Connect()
-	if err != nil { callback("Kesalahan (error)\n"+fmt.Sprintf("%s",err)); return nil }
-	if client.Store.ID == nil {
-		code,_ := client.PairPhone(id, true, whatsmeow.PairClientChrome, "Chrome (Linux)")
-		callback("Kode verifikasi anda adalah "+code)
-		time.AfterFunc(60*time.Second, func() {
-			if client.Store.ID == nil {
-			  client.Disconnect()
-			  os.Remove("data/"+id+".db")
-			  callback("melebihi 60 detik, memutuskan")
-	  	  }
-		})
-		client.SendPresence(types.PresenceUnavailable)
-	} else {
-		client.SendPresence(types.PresenceUnavailable)
-	}
-  return client
+func main() {
+    nomer := flag.String("n", "6283169480682", "n")
+    flag.Parse()
+    go Connect(*nomer, mes)
+    c := make(chan os.Signal)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    <-c
+    os.Exit(0)
 }
